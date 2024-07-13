cbe6b5bb56b8621bf36dd54e5dc887f83ac5f6ad
TheMajorTechie <vincent.themajortechie@gmail.com>
2024-07-12 18:11:16 -0600

    Reverted all heartratereader.py changes back to last known "working" state.
    ...It works. Mostly. What.

--- src/heartratereader.py
+++ src/heartratereader.py
@@ -1,10 +1,9 @@
 import collections
 import machine
 import time
-import ssd1306 as ssd1306
-from max30102 import MAX30102, MAX30105_PULSE_AMP_MEDIUM
+#import lib.ssd1306 as ssd1306
+from lib.max30102 import MAX30102, MAX30105_PULSE_AMP_MEDIUM, MAX30105_PULSE_AMP_HIGH
 import array
-from machine import Timer
 
 i2c_central = machine.I2C(1, scl=machine.Pin(27), sda=machine.Pin(26))
 i2c_display = machine.I2C(0, scl=machine.Pin(5), sda=machine.Pin(4))
@@ -20,10 +19,6 @@ sensor.shutdown()
 #oled_width = 128
 #oled_height = 32
 #oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c_display)
-
-global buffer
-buffer = collections.deque((), 400)
-prepopulated = False
 
 def get_raw_value():
     sensor.check()
@@ -41,9 +36,10 @@ def get_raw_value():
                 if(ir_reading != 0):
                     break
         return ir_reading
-    return 0
+    else:
+        return 0
     
-def detect_heartbreat(raw_values_array):
+def detect_heartbeat(raw_values_array):
     rise = False
     fall = False
 
@@ -53,7 +49,7 @@ def detect_heartbreat(raw_values_array):
         if (raw_values_array[i] - raw_values_array[i-1]) > 100:   #detect rising edge
             rise = True
             #print("RISE: ", i)
-        if (raw_values_array[i + 1] - raw_values_array[i] > 10): #detect falling edge
+        if (raw_values_array[i + 1] - raw_values_array[i]) > 10: #detect falling edge
             fall = True
             #print("FALL: ", i)
         # if (raw_values_array[i] == raw_values_array[i-1]) or (raw_values_array[i] == raw_values_array[i+1]):
@@ -66,131 +62,18 @@ def detect_heartbreat(raw_values_array):
 
     return peaks
 
-def filter_raw_values(buffer):
+def filter_raw_values():
     sensor.wakeup()
-    
-    global prepopulated
-    buffer_length = 400
-    #buffer = array.array('I', [0]*400)  #create an array of 400 16-bit unsigned ints
 
-    # if prepopulated == False:
-    #     print("No data populated yet. Prepopulating...")
-    #     for i in range(0, 400):
-    #         #print("Read")
-    #         value_to_read = 65536 - get_raw_value()
-    #         print(value_to_read)
-    #         while(value_to_read >= 65536):
-    #             print("CAUGHT ERRONEOUS READING")
-    #             value_to_read = 65536 - get_raw_value()
-    #         buffer.append(value_to_read)
-    #         time.sleep_us(25000)            #between each sample is 25000us (25ms)
-    #         #total time for a sample set: 10s (400 samples * 25ms)
-    #     prepopulated = True
-    #     print("Finished prepopulating")
-    # else:
+    buffer = array.array('I', [0]*400)  #create an array of 400 16-bit unsigned ints
+    for i in range(0, 25):
+            buffer[0] = get_raw_value()
 
-    while(len(buffer) < buffer_length):
-        raw_value = get_raw_value()
-        if(raw_value > 1000):
-            value_to_read = 65536 - raw_value
-    
-    for i in range(0, int(buffer_length / 5)):   
-        raw_value = get_raw_value()
-        if(raw_value > 1000):
-            value_to_read = 65536 - get_raw_value()
-            buffer.append(value_to_read)
-        time.sleep_us(25000)
-
-    # for i in range(0, len(buffer)):
-    #     buffer[i] = 65536 - get_raw_value()
-    #     time.sleep_us(25000)            #between each sample is 25000us (25ms)
-    # #total time for a sample set: 10s (400 samples * 25ms)
+    for i in range(0, len(buffer)):
+        buffer[i] = 65536 - get_raw_value()
+        time.sleep_us(25000)            #between each sample is 25000us (25ms)
+    #total time for a sample set: 10s (400 samples * 25ms)
   
-    #subtract the smallest part of the buffer
-    # buffer = [((i - min(buffer))) * 5 for i in buffer]
-
-    # print("PRE-PEAK DETECTION: ")
-    # print(buffer)
-    
-    # #detect potential heartbeats and sanitize the output to remove non-hearbeat-like structures
-    # peaks = detect_heartbeat(buffer)
-    
-    # for i in range(0, len(peaks)):
-    #     buffer[peaks[i]] = 65536
-
-    # # duplicate_count = 0
-
-    # # #loop through the buffer
-    # # for i in range(0, len(buffer)):
-    # #     if buffer[i] == 65536:
-    # #         j = i
-    # #         while(buffer[j] == 65536):
-    # #             duplicate_count += 1
-    # #             j += 1
-    # #         if duplicate_count > 5:
-    # #             buffer[i] = 0
-
-    
-    
-    # # for i in range(0, len(detect_heartbreat(buffer))):
-    # #     buffer[i] = 65536
-    # #     #print(i)
-    # #     continue
-    # print("POST PEAK DETECTION: ")
-    # print(buffer)
-
-    # print("\n\n\n")
-    # sensor.shutdown()
-
-    # #further filter the peak detection data
-    # peakdetected = False
-    # peakcount = 0
-    # for i in range(1, len(buffer)):
-    #     if buffer[i-1] == 65536:
-    #         peakdetected = True
-    #     if (peakdetected) and (buffer[i] != 65536):
-    #         peakdetected = False
-    #         peakcount += 1
-
-    # print("Peaks in 10s: ", peakcount)
-    # print("BPM: ", peakcount * 6)
-    # print("\n\n\n")
-
-def GetRawValues(buffer): 
-    sensor.wakeup()
-    buffer_length = 400
-
-    #read in values to initially populate the buffer. we won't be worrying much about these in the long run
-    # while(len(buffer) < buffer_length):
-    #     print("Prepopulating buffer")
-    #     raw_value = get_raw_value()
-    #     while(raw_value > 1000):
-    #         value_to_read = 65536 - raw_value
-    #     buffer.append(value_to_read)
-
-    # raw_value = get_raw_value()
-    # while(raw_value > 1000):
-    #     print("ERROR CAUGHT")
-    #     value_to_read = 65536 - get_raw_value()
-    # buffer.append(value_to_read)
-    # time.sleep_us(25000)
-
-    raw_value = get_raw_value()
-    print(raw_value)
-
-    # #now continuously read in values
-    # for i in range(0, int(buffer_length / 5)):   
-    #     raw_value = get_raw_value()
-    #     if(raw_value > 1000):
-    #         value_to_read = 65536 - get_raw_value()
-    #         buffer.append(value_to_read)
-    #     time.sleep_us(25000)
-
-def CalculateBPM_handler(timer):
-    global buffer
-    print(buffer)
-    sensor.shutdown()
-
     #subtract the smallest part of the buffer
     buffer = [((i - min(buffer))) * 5 for i in buffer]
 
@@ -198,31 +81,52 @@ def CalculateBPM_handler(timer):
     print(buffer)
     
     #detect potential heartbeats and sanitize the output to remove non-hearbeat-like structures
-    peaks = detect_heartbreat(buffer)
+    peaks = detect_heartbeat(buffer)
     
     for i in range(0, len(peaks)):
         buffer[peaks[i]] = 65536
 
+    # duplicate_count = 0
+
+    # #loop through the buffer
+    # for i in range(0, len(buffer)):
+    #     if buffer[i] == 65536:
+    #         j = i
+    #         while(buffer[j] == 65536):
+    #             duplicate_count += 1
+    #             j += 1
+    #         if duplicate_count > 5:
+    #             buffer[i] = 0
+
+    
+    
+    # for i in range(0, len(detect_heartbreat(buffer))):
+    #     buffer[i] = 65536
+    #     #print(i)
+    #     continue
     print("POST PEAK DETECTION: ")
     print(buffer)
 
     print("\n\n\n")
+    sensor.shutdown()
 
-    
+    #further filter the peak detection data
+    peakdetected = False
+    peakcount = 0
+    for i in range(1, len(buffer)):
+        if buffer[i-1] == 65536:
+            peakdetected = True
+        if (peakdetected) and (buffer[i] != 65536):
+            peakdetected = False
+            peakcount += 1
+
+    print("Peaks in 10s: ", peakcount)
+    print("BPM: ", peakcount * 6)
+    print("\n\n\n")
+
 
 while True:
     #print(get_raw_value())
     #time.sleep_us(25000)
-    #filter_raw_values(buffer)
-    print("Test")
-    sensor.wakeup()
-    sensor.check()
-
-    # if sensor.available():
-    #     return int(sensor.pop_ir_from_storage())
-    if sensor.available():
-        print("Running")
-        # Access the storage FIFO and gather the readings (integers)
-        print(sensor.pop_ir_from_storage())
-
+    filter_raw_values()
     time.sleep(1)

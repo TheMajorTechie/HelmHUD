5587bda84c51288bafaa2883c711cf211583dae3
TheMajorTechie <vincent.themajortechie@gmail.com>
2024-07-27 12:35:42 -0600

    Added some additional comments into main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ i2c_central = machine.I2C(1, scl=machine.Pin(27), sda=machine.Pin(26))
 
 envSensors = envsense_wrapper.EnvSenseWrapper(i2c_central)          #this is the central environmental sensor wrapper!
 
+#===============================================getting sensor readouts
+
 bme = envSensors.read_out_bme()
 pressure = round(bme[0], 2) 
 temp = round(bme[1], 2) 
@@ -19,6 +21,8 @@ gas = sgp[0]
 voc = sgp[1]
 icm = envSensors.read_out_9dof()
 
+#=============================================printing sensor readouts
+
 print("==================================================")
 print("pressure : %7.2f hPa" %pressure)
 print("temp : %-6.2f ℃" %temp)
@@ -30,3 +34,5 @@ print("VOC : %d " %voc)
 print("Acceleration: X = %d, Y = %d, Z = %d" %(icm[0],icm[1],icm[2]))
 print("Gyroscope:     X = %d , Y = %d , Z = %d" %(icm[3],icm[4],icm[5]))
 print("Magnetic:      X = %d , Y = %d , Z = %d" %(icm[6],icm[7],icm[8]))
+
+print("Main.py finished execution!")

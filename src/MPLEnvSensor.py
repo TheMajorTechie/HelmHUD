73a58e784f8587aea40c1775241d0927cfb08781
DuckKee <sthing2002@gmail.com>
2024-08-02 14:31:25 -0600

    Added the library and code for the Temp/Pressure/Alt sensor

--- src/MPLEnvSensor.py
+++ src/MPLEnvSensor.py
@@ -1,17 +1,11 @@
 import time
-import board
-from lib.MPL import adafruit_mpl3115a2
+
+from micropython_mpl3115a2 import mpl3115a2
 
 from machine import Pin, I2C
 i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=400000)
 
-# Create sensor object, communicating over the board's default I2C bus
-#i2c = board.I2C()  # uses board.SCL and board.SDA
-
-# Initialize the MPL3115A2.
-sensor = adafruit_mpl3115a2.MPL3115A2(i2c)
-
-sensor.sealevel_pressure = 1022.5
+sensor = mpl3115a2.MPL3115A2(i2c)
 
 # Reads the sensor values and print them every second.
 while True:

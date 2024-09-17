e3d58ced429ea6bae4d2f03019d3f0a67969f773
TheMajorTechie <vincent.themajortechie@gmail.com>
2024-09-17 14:01:14 -0600

    Cleaned up some files and renamed bt_send to main.py

--- main.py
+++ main.py
@@ -1,124 +1,114 @@
-# This code is based partly on ble_simple_peripheral from the Micropython examples repository.
-# Documentation on Bluetooth transmission: https://docs.micropython.org/en/latest/library/bluetooth.html#event-handling
+# Adapted from https://github.com/micropython/micropython/blob/master/examples/bluetooth/ble_uart_peripheral.py
 
 import bluetooth
-import struct
 from lib.ble_advertising import advertising_payload
+
 from micropython import const
-import time
 
-# IRQ events
-_IRQ_CENTRAL_CONNECT = const(1)		# central device connected
-_IRQ_CENTRAL_DISCONNECT = const(2)	# central device disconnected
-_IRQ_GATTS_WRITE = const(3)			# client has written to the characteristic/descriptor
+_IRQ_CENTRAL_CONNECT = const(1)
+_IRQ_CENTRAL_DISCONNECT = const(2)
+_IRQ_GATTS_WRITE = const(3)
 
-#Flags for characteristics and descriptors
-_FLAG_READ = const(0x0002)
-_FLAG_WRITE_NO_RESPONSE = const(0x0004)
 _FLAG_WRITE = const(0x0008)
 _FLAG_NOTIFY = const(0x0010)
 
-#DO NOT CHANGE THESE UUIDs.
 _UART_UUID = bluetooth.UUID("6E400001-B5A3-F393-E0A9-E50E24DCCA9E")
 _UART_TX = (
     bluetooth.UUID("6E400003-B5A3-F393-E0A9-E50E24DCCA9E"),
-    _FLAG_READ | _FLAG_NOTIFY,
+    _FLAG_NOTIFY,
 )
 _UART_RX = (
     bluetooth.UUID("6E400002-B5A3-F393-E0A9-E50E24DCCA9E"),
-    _FLAG_WRITE | _FLAG_WRITE_NO_RESPONSE,
+    _FLAG_WRITE,
 )
 _UART_SERVICE = (
     _UART_UUID,
     (_UART_TX, _UART_RX),
 )
 
-class HelmHUDUART:
-    def __init__(self, ble, name="HH-uart"):   #register BLE UART services
+# org.bluetooth.characteristic.gap.appearance.xml
+_ADV_APPEARANCE_GENERIC_COMPUTER = const(128)
+
+
+class BLEUART:        
+    def __init__(self, ble, name="HH-uart", rxbuf=256):   #register BLE UART services
         self._ble = ble
         self._ble.active(True)
         self._ble.irq(self._irq)
-        ((self._handle_tx, self._handle_rx),) = self._ble.gatts_register_services((_UART_SERVICE,))
         ((self._tx_handle, self._rx_handle),) = self._ble.gatts_register_services((_UART_SERVICE,))
         self._connections = set()
+        self._ble.config(mtu=256)							#increase the number of bytes that can be sent from the default of 20
         self._write_callback = None
         self._payload = advertising_payload(name=name, services=[_UART_UUID])
         self._advertise()
+
+    def irq(self, handler):
+        self._handler = handler
 
     def _irq(self, event, data):
         # Track connections so we can send notifications.
         if event == _IRQ_CENTRAL_CONNECT:
             conn_handle, _, _ = data
-            print("New connection", conn_handle)
             self._connections.add(conn_handle)
         elif event == _IRQ_CENTRAL_DISCONNECT:
             conn_handle, _, _ = data
-            print("Disconnected", conn_handle)
-            self._connections.remove(conn_handle)
+            if conn_handle in self._connections:
+                self._connections.remove(conn_handle)
             # Start advertising again to allow a new connection.
             self._advertise()
         elif event == _IRQ_GATTS_WRITE:
             conn_handle, value_handle = data
-            value = self._ble.gatts_read(value_handle)
-            if value_handle == self._handle_rx and self._write_callback:
-                self._write_callback(value)
+            if conn_handle in self._connections and value_handle == self._rx_handle:
+                self._rx_buffer += self._ble.gatts_read(self._rx_handle)
+                if self._handler:
+                    self._handler()
 
-    def send(self, data):
-        #self._ble.gatts_write(self._handle_tx, struct.pack("<s", data))
-        testPayload = "someData"
-        
-        #rawBytes=bytes(testPayload, 'utf-8')
-        #packed =struct.pack("I%ds" % (len(rawBytes),), len(rawBytes), rawBytes)
-        
-        #self._ble.gatts_write(self._handle_tx, struct.pack("<s", packed))
-        
-        #self.uart.wri
-        
-        for conn_handle in self._connections:
-            self._ble.gatts_notify(conn_handle, self._handle_tx)
+    def any(self):
+        return len(self._rx_buffer)
 
-        
-        #for conn_handle in self._connections:
-        #    self._ble.gatts_notify(conn_handle, self._handle_tx, data)
+    def read(self, sz=None):
+        if not sz:
+            sz = len(self._rx_buffer)
+        result = self._rx_buffer[0:sz]
+        self._rx_buffer = self._rx_buffer[sz:]
+        return result
 
-    def is_connected(self):
-        return len(self._connections) > 0
-
-    def _advertise(self, interval_us=500000):
-        print("Starting advertising")
-        self._ble.gap_advertise(interval_us, adv_data=self._payload)
-
-    def on_write(self, callback):
-        self._write_callback = callback
-        
     def write(self, data):
         for conn_handle in self._connections:
             self._ble.gatts_notify(conn_handle, self._tx_handle, data)
 
-def main():
-    print("Attempting to connect.")
+    def close(self):
+        for conn_handle in self._connections:
+            self._ble.gap_disconnect(conn_handle)
+        self._connections.clear()
+
+    def _advertise(self, interval_us=500000):
+        self._ble.gap_advertise(interval_us, adv_data=self._payload)
+
+
+def demo():
+    import time
     ble = bluetooth.BLE()
-    p = HelmHUDUART(ble)
-
+    uart = BLEUART(ble)
     import wired
+    #a dummy sensor data payload terminated with a newline
+    sensorArray = [[813.79, 26.16, 23.84], 12.56, 0, [30779, 0], [-556, 144, -16168, 19, 70, 33, 9472, 36096, 56318], 0]
+    packedArray = str(sensorArray)+"\n"
 
-    def rx_handler(rxdata):
-        print("Received: ", rxdata)
+    def on_rx():
+        print("rx: ", uart.read().decode().strip())
 
-    p.on_write(rx_handler)
+    uart.irq(handler=on_rx)
 
     try:
         while True:
-            if p.is_connected():
-                #p.send(wired.packedArray)
-                #p.write(wired.packedArray)
-                p.write("BME:" + wired.bme)
-                time.sleep_ms(1000)
-
+            uart.write(packedArray)
+            #uart.write(str(wired.bme))
     except KeyboardInterrupt:
         pass
 
-    #uart.close()
-        
+    uart.close()
+
+
 if __name__ == "__main__":
-    main()
+    demo()
